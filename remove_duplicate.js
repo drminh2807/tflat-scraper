9008f46f6a36e91fc3a2fe2bc887b5f8ce6a0499
Robert Go <robert.minh.goldenowl@gmail.com>
2023-03-18 14:11:28 +0700

    Folder struct

--- remove_duplicate.js
+++
@@ -1,7 +0,0 @@
-
-const fs = require('fs');
-const raw = fs.readFileSync('./oxford.csv', { encoding: 'utf8', flag: 'r' });
-const lines = raw.split('\n')
-const set = new Set(lines)
-const newLines = [...set].join('\n')
-fs.writeFileSync('./oxford_remove_duplicate.csv', newLines)
